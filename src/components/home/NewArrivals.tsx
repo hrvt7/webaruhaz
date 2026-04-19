ccfac28c4620efdf51a8c917f33aa11b99b5baca
hrvthadam7-dotcom <hrvthadam.920727@gmail.com>
2026-04-19 15:35:52 +0200

    fix: új érkezések carousel középre snap, mindkét oldalon peek

--- src/components/home/NewArrivals.tsx
+++ src/components/home/NewArrivals.tsx
@@ -24,17 +24,17 @@ export default function NewArrivals({ items }: { items: Product[] }) {
             {t.home.viewAll}
           </Link>
         </div>
+      </div>
 
-        <div className="flex gap-4 md:gap-6 overflow-x-auto no-scrollbar -mx-6 md:mx-0 px-6 md:px-0 snap-x snap-mandatory">
-          {items.map((p) => (
-            <div
-              key={p.slug}
-              className="shrink-0 snap-start w-[70%] sm:w-[45%] md:w-[30%] lg:w-[23%]"
-            >
-              <ProductCard p={p} />
-            </div>
-          ))}
-        </div>
+      <div className="flex gap-4 md:gap-6 overflow-x-auto no-scrollbar snap-x snap-mandatory px-[15vw] sm:px-[27.5vw] md:px-[35vw] lg:px-[38.5vw]">
+        {items.map((p) => (
+          <div
+            key={p.slug}
+            className="shrink-0 snap-center w-[70vw] sm:w-[45vw] md:w-[30vw] lg:w-[23vw]"
+          >
+            <ProductCard p={p} />
+          </div>
+        ))}
       </div>
     </section>
   );
